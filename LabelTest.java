4a1307162ee41a3627ab1ee4d8ac155b8dcd48fd
naahe25 <naahebd25@gmail.com>
2023-11-27 20:59:05 +0600

    new files added

---
+++ LabelTest.java
@@ -0,0 +1,12 @@
+
+import javax.swing.JFrame;
+
+public class LabelTest {
+    public static void main(String[] args) {
+        LabelFrame labelFrame = new LabelFrame();
+        labelFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        labelFrame.setSize(1300, 1300);
+        labelFrame.setVisible(true);
+    }
+
+}
---
+++ TextFieldTest.java
@@ -0,0 +1,8 @@
+public class TextFieldTest {
+    public static void main(String[] args) {
+        TextFieldFrame textFieldFrame = new TextFieldFrame();
+        textFieldFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        textFieldFrame.setSize(350, 100);
+        textFieldFrame.setVisible(true);
+    }
+}
